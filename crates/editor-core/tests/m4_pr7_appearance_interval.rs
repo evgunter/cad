415701d73995df32b9472ab3075aa040798599a1
Evan Ryan Gunter <evgunter@gmail.com>
2026-07-28 17:19:37 -0700

    fmt: wrap the widened interval test-helper signatures

--- crates/editor-core/tests/m4_pr7_appearance_interval.rs
+++ crates/editor-core/tests/m4_pr7_appearance_interval.rs
@@ -42,7 +42,9 @@ fn block(
     )
 }
 
-fn run<T: Decide + editor_core::ContentBits + geom_core::Bounds + Send + Sync>(doc: &ProfileDoc) -> Evaluation<T> {
+fn run<T: Decide + editor_core::ContentBits + geom_core::Bounds + Send + Sync>(
+    doc: &ProfileDoc,
+) -> Evaluation<T> {
     evaluate::<T>(doc, None, &CancelToken::new(), &EvalOptions::default())
 }
 
